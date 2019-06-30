5efe6304f2eef0c376ac098bafe5b439b9c9ec08
Guillaume Pinot <texitoi@texitoi.eu>
2019-06-30 19:12:14 +0200

    update README.md for 0.3
    fix #208

--- examples/basic.rs
+++ examples/basic.rs
@@ -23,18 +23,17 @@ use std::path::PathBuf;
 #[clap(name = "basic")]
 struct Opt {
     // A flag, true if used in the command line. Note doc comment will
-    // be used for the help message of the flag.
+    // be used for the help message of the flag. The name of the
+    // argument will be, by default, based on the name of the field.
     /// Activate debug mode
-    #[clap(short = "d", long = "debug")]
+    #[clap(short, long)]
     debug: bool,
 
     // The number of occurrences of the `v/verbose` flag
     /// Verbose mode (-v, -vv, -vvv, etc.)
-    #[clap(short = "v", long = "verbose", parse(from_occurrences))]
+    #[clap(short, long, parse(from_occurrences))]
     verbose: u8,
 
-    // The standalone parameters `short`and `long` automatically name the
-    // arguments based on the field name (defaults to kebab-case).
     /// Set speed
     #[clap(short, long, default_value = "42")]
     speed: f64,
@@ -43,6 +42,8 @@ struct Opt {
     #[clap(short, long, parse(from_os_str))]
     output: PathBuf,
 
+    // the long option will be translated by default to kebab case,
+    // i.e. `--nb-cars`.
     /// Number of cars
     #[clap(short = "c", long)]
     nb_cars: Option<i32>,
@@ -58,5 +59,5 @@ struct Opt {
 
 fn main() {
     let opt = Opt::parse();
-    println!("{:?}", opt);
+    println!("{:#?}", opt);
 }
