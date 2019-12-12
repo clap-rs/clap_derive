e8435b2eedbd5f1fa4dc90b7e67e8e4ccf7ff888
csrene <renecoutoesilva@gmail.com>
2019-12-11 21:37:50 -0300

    fixed typo, removed misleading doc

--- examples/group.rs
+++ examples/group.rs
@@ -5,12 +5,12 @@ extern crate clap;
 use clap::{Clap, ArgGroup};
 
 #[derive(Clap, Debug)]
-#[clap(group = ArgGroup::with_name("vers").required(true))]
+#[clap(group = ArgGroup::with_name("verb").required(true))]
 struct Opt {
     /// Set a custom HTTP verb
     #[clap(long, group = "verb")]
     method: Option<String>,
-    /// HTTP GET; default if no other HTTP verb is selected
+    /// HTTP GET
     #[clap(long, group = "verb")]
     get: bool,
     /// HTTP HEAD
