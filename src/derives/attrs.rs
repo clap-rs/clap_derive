TeXitoi/structopt

Fix invalid structopt syntax error message (#154)
There was a typo preventing the probematic attr to be shown to the user.

structopt-derive/src/attrs.rs
@@ -155,7 +155,7 @@ impl Attrs {
                 match quote!(#path).to_string().as_ref() {
                     "structopt" => Some(
                         attr.interpret_meta()
-                            .expect(&format!("invalid structopt syntax: {}", quote!(attr))),
+                            .expect(&format!("invalid structopt syntax: {}", quote!(#attr))),
                     ),
                     _ => None,
                 }

clap-rs/clap_derive

src/derives/attrs.rs
@@ -173,7 +173,7 @@ impl Attrs {
                 match quote!(#path).to_string().as_ref() {
                     "clap" => Some(
                         attr.interpret_meta()
-                            .expect(&format!("invalid clap_derive syntax: {}", quote!(attr))),
+                            .expect(&format!("invalid clap_derive syntax: {}", quote!(#attr))),
                     ),
                     _ => None,
                 }