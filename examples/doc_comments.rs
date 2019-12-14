TeXitoi/structopt

Introduce smarter parsing of doc comments. (#146)

examples/doc_comments.rs
@@ -1,0 +1,86 @@
+// Copyright 2018 Guillaume Pinot (@TeXitoi) <texitoi@texitoi.eu>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#[macro_use]
+extern crate structopt;
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// A basic example for the usage of doc comments as replacement
+/// of the arguments `help`, `long_help`, `about` and `long_about`.
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    /// Just use doc comments to replace `help`, `long_help`,
+    /// `about` or `long_about` input.
+    #[structopt(short = "f", long = "first-flag")]
+    first_flag: bool,
+
+    /// Split between `help` and `long_help`.
+    ///
+    /// In the previous case structopt is going to present
+    /// the whole comment both as text for the `help` and the
+    /// `long_help` argument.
+    ///
+    /// But if the doc comment is formatted like this example
+    /// -- with an empty second line splitting the heading and
+    /// the rest of the comment -- only the first line is used
+    /// as `help` argument. The `long_help` argument will still
+    /// contain the whole comment.
+    ///
+    /// ## Attention
+    ///
+    /// Any formatting next to empty lines that could be used
+    /// inside a doc comment is currently not preserved. If
+    /// lists or other well formatted content is required it is
+    /// necessary to use the related structopt argument with a
+    /// raw string as shown on the `third_flag` description.
+    #[structopt(short = "s", long = "second-flag")]
+    second_flag: bool,
+
+    #[structopt(
+        short = "t",
+        long = "third-flag",
+        long_help = r"This is a raw string.
+
+It can be used to pass well formatted content (e.g. lists or source
+code) in the description:
+
+ - first example list entry
+ - second example list entry
+ "
+    )]
+    third_flag: bool,
+
+    #[structopt(subcommand)]
+    sub_command: SubCommand,
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt()]
+enum SubCommand {
+    /// The same rules described previously for flags. Are
+    /// also true for in regards of sub-commands.
+    #[structopt(name = "first")]
+    First,
+
+    /// Applicable for both `about` an `help`.
+    ///
+    /// The formatting rules described in the comment of the
+    /// `second_flag` also apply to the description of
+    /// sub-commands which is normally given through the `about`
+    /// and `long_about` arguments.
+    #[structopt(name = "second")]
+    Second,
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+}

tests/doc-comments-help.rs
@@ -70,3 +70,79 @@ fn empty_line_in_doc_comment_is_double_linefeed() {
     println!("{}", output);
     assert!(output.starts_with("lorem-ipsum \nFoo.\n\nBar\n\nUSAGE:"));
 }
+
+#[test]
+fn splits_flag_doc_comment_between_short_and_long() {
+    /// Lorem ipsumclap
+    #[derive(StructOpt, PartialEq, Debug)]
+    #[structopt(name = "lorem-ipsum", about = "Dolor sit amet")]
+    struct LoremIpsum {
+        /// DO NOT PASS A BAR UNDER ANY CIRCUMSTANCES
+        ///
+        /// Or something else
+        #[structopt(long = "foo")]
+        foo: bool,
+    }
+
+    let mut app = LoremIpsum::clap();
+
+    let short_help = {
+        let mut buffer = Vec::new();
+        app.write_help(&mut buffer).ok();
+
+        String::from_utf8(buffer).unwrap()
+    };
+
+    let long_help = {
+        let mut buffer = Vec::new();
+        app.write_long_help(&mut buffer).ok();
+
+        String::from_utf8(buffer).unwrap()
+    };
+
+    assert!(!short_help.contains("Or something else"));
+    assert!(long_help.contains("DO NOT PASS A BAR UNDER ANY CIRCUMSTANCES"));
+    assert!(long_help.contains("Or something else"));
+}
+
+#[test]
+fn splits_subcommand_doc_comment_between_short_and_long() {
+    /// Lorem ipsumclap
+    #[derive(StructOpt, Debug)]
+    #[structopt(name = "lorem-ipsum", about = "Dolor sit amet")]
+    struct LoremIpsum {
+        #[structopt(subcommand)]
+        foo: SubCommand,
+    }
+
+    #[derive(StructOpt, Debug)]
+    pub enum SubCommand {
+        /// DO NOT PASS A BAR UNDER ANY CIRCUMSTANCES
+        ///
+        /// Or something else
+        #[structopt(name = "foo")]
+        Foo {
+            #[structopt(help = "foo")]
+            bars: Vec<String>,
+        },
+    }
+
+    let app = LoremIpsum::clap();
+
+    let short_help = {
+        let mut buffer = Vec::new();
+        app.write_help(&mut buffer).ok();
+
+        String::from_utf8(buffer).unwrap()
+    };
+
+    let long_help = {
+        app.get_matches_from_safe(vec!["test", "foo", "--help"])
+            .expect_err("")
+            .message
+    };
+
+    assert!(!short_help.contains("Or something else"));
+    assert!(long_help.contains("DO NOT PASS A BAR UNDER ANY CIRCUMSTANCES"));
+    assert!(long_help.contains("Or something else"));
+}

clap-rs/clap_derive

examples/doc_comments.rs
@@ -1,0 +1,86 @@
+// Copyright 2018 Guillaume Pinot (@TeXitoi) <texitoi@texitoi.eu>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#[macro_use]
+extern crate clap;
+
+use std::path::PathBuf;
+use clap::Clap;
+
+/// A basic example for the usage of doc comments as replacement
+/// of the arguments `help`, `long_help`, `about` and `long_about`.
+#[derive(Clap, Debug)]
+#[clap(name = "basic")]
+struct Opt {
+    /// Just use doc comments to replace `help`, `long_help`,
+    /// `about` or `long_about` input.
+    #[clap(short = "f", long = "first-flag")]
+    first_flag: bool,
+
+    /// Split between `help` and `long_help`.
+    ///
+    /// In the previous case clap is going to present
+    /// the whole comment both as text for the `help` and the
+    /// `long_help` argument.
+    ///
+    /// But if the doc comment is formatted like this example
+    /// -- with an empty second line splitting the heading and
+    /// the rest of the comment -- only the first line is used
+    /// as `help` argument. The `long_help` argument will still
+    /// contain the whole comment.
+    ///
+    /// ## Attention
+    ///
+    /// Any formatting next to empty lines that could be used
+    /// inside a doc comment is currently not preserved. If
+    /// lists or other well formatted content is required it is
+    /// necessary to use the related clap argument with a
+    /// raw string as shown on the `third_flag` description.
+    #[clap(short = "s", long = "second-flag")]
+    second_flag: bool,
+
+    #[clap(
+        short = "t",
+        long = "third-flag",
+        long_help = r"This is a raw string.
+
+It can be used to pass well formatted content (e.g. lists or source
+code) in the description:
+
+ - first example list entry
+ - second example list entry
+ "
+    )]
+    third_flag: bool,
+
+    #[clap(subcommand)]
+    sub_command: SubCommand,
+}
+
+#[derive(Clap, Debug)]
+#[clap()]
+enum SubCommand {
+    /// The same rules described previously for flags. Are
+    /// also true for in regards of sub-commands.
+    #[clap(name = "first")]
+    First,
+
+    /// Applicable for both `about` an `help`.
+    ///
+    /// The formatting rules described in the comment of the
+    /// `second_flag` also apply to the description of
+    /// sub-commands which is normally given through the `about`
+    /// and `long_about` arguments.
+    #[clap(name = "second")]
+    Second,
+}
+
+fn main() {
+    let opt = Opt::parse();
+    println!("{:?}", opt);
+}

tests/doc-comments-help.rs
@@ -8,7 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 //
-// This work was derived from Structopt (https://github.com/TeXitoi/structopt)
+// This work was derived from Structopt (https://github.com/TeXitoi/clap)
 // commit#ea76fa1b1b273e65e3b0b1046643715b49bec51f which is licensed under the
 // MIT/Apache 2.0 license.
 
@@ -79,4 +79,82 @@ fn empty_line_in_doc_comment_is_double_linefeed() {
 
     println!("{}", output);
     assert!(output.starts_with("lorem-ipsum \nFoo.\n\nBar\n\nUSAGE:"));
+}
+
+#[test]
+fn splits_flag_doc_comment_between_short_and_long() {
+    use clap::IntoApp;
+    /// Lorem ipsumclap
+    #[derive(Clap, PartialEq, Debug)]
+    #[clap(name = "lorem-ipsum", about = "Dolor sit amet")]
+    struct LoremIpsum {
+        /// DO NOT PASS A BAR UNDER ANY CIRCUMSTANCES
+        ///
+        /// Or something else
+        #[clap(long = "foo")]
+        foo: bool,
+    }
+
+    let mut app = LoremIpsum::into_app();
+
+    let short_help = {
+        let mut buffer = Vec::new();
+        app.write_help(&mut buffer).ok();
+
+        String::from_utf8(buffer).unwrap()
+    };
+
+    let long_help = {
+        let mut buffer = Vec::new();
+        app.write_long_help(&mut buffer).ok();
+
+        String::from_utf8(buffer).unwrap()
+    };
+
+    assert!(!short_help.contains("Or something else"));
+    assert!(long_help.contains("DO NOT PASS A BAR UNDER ANY CIRCUMSTANCES"));
+    assert!(long_help.contains("Or something else"));
+}
+
+#[test]
+fn splits_subcommand_doc_comment_between_short_and_long() {
+    use clap::IntoApp;
+    /// Lorem ipsumclap
+    #[derive(Clap, Debug)]
+    #[clap(name = "lorem-ipsum", about = "Dolor sit amet")]
+    struct LoremIpsum {
+        #[clap(subcommand)]
+        foo: SubCommand,
+    }
+
+    #[derive(Clap, Debug)]
+    pub enum SubCommand {
+        /// DO NOT PASS A BAR UNDER ANY CIRCUMSTANCES
+        ///
+        /// Or something else
+        #[clap(name = "foo")]
+        Foo {
+            #[clap(help = "foo")]
+            bars: Vec<String>,
+        },
+    }
+
+    let mut app = LoremIpsum::into_app();
+
+    let short_help = {
+        let mut buffer = Vec::new();
+        app.write_help(&mut buffer).ok();
+
+        String::from_utf8(buffer).unwrap()
+    };
+
+    let long_help = {
+        app.try_get_matches_from(vec!["test", "foo", "--help"])
+            .expect_err("")
+            .message
+    };
+
+    assert!(!short_help.contains("Or something else"));
+    assert!(long_help.contains("DO NOT PASS A BAR UNDER ANY CIRCUMSTANCES"));
+    assert!(long_help.contains("Or something else"));
 }