TeXitoi/structopt

Small fixes: clippy and typos (#215)

structopt-derive/src/parse.rs
@@ -131,21 +131,18 @@ pub fn parse_structopt_attributes(all_attrs: &[Attribute]) -> Vec<StructOptAttr>
     let mut s_opt_attrs: Vec<StructOptAttr> = vec![];
     for attr in all_attrs {
         let path = &attr.path;
-        match quote!(#path).to_string().as_ref() {
-            "structopt" => {
-                let tokens = attr.tts.clone();
-                let is_empty = tokens.is_empty();
-                let so_attrs: StructOptAttributes = syn::parse2(tokens).unwrap_or_else(|err| {
-                    let tokens_str = if is_empty {
-                        String::new()
-                    } else {
-                        format!("problematic tokens: {}", &attr.tts)
-                    };
-                    panic!("{}, {}", err.to_string(), tokens_str)
-                });
-                s_opt_attrs.extend(so_attrs.attrs);
-            }
-            _ => {}
+        if let "structopt" = quote!(#path).to_string().as_ref() {
+            let tokens = attr.tts.clone();
+            let is_empty = tokens.is_empty();
+            let so_attrs: StructOptAttributes = syn::parse2(tokens).unwrap_or_else(|err| {
+                let tokens_str = if is_empty {
+                    String::new()
+                } else {
+                    format!("problematic tokens: {}", &attr.tts)
+                };
+                panic!("{}, {}", err.to_string(), tokens_str)
+            });
+            s_opt_attrs.extend(so_attrs.attrs);
         }
     }
     s_opt_attrs

clap-rs/clap_derive

src/derives/parse.rs
@@ -130,21 +130,18 @@ pub fn parse_clap_attributes(all_attrs: &[Attribute]) -> Vec<ClapAttr> {
     let mut s_opt_attrs: Vec<ClapAttr> = vec![];
     for attr in all_attrs {
         let path = &attr.path;
-        match quote!(#path).to_string().as_ref() {
-            "clap" => {
-                let tokens = attr.tts.clone();
-                let is_empty = tokens.is_empty();
-                let so_attrs: ClapAttributes = syn::parse2(tokens).unwrap_or_else(|err| {
-                    let tokens_str = if is_empty {
-                        String::new()
-                    } else {
-                        format!("problematic tokens: {}", &attr.tts)
-                    };
-                    panic!("{}, {}", err.to_string(), tokens_str)
-                });
-                s_opt_attrs.extend(so_attrs.attrs);
-            }
-            _ => {}
+        if let "clap" = quote!(#path).to_string().as_ref() {
+            let tokens = attr.tts.clone();
+            let is_empty = tokens.is_empty();
+            let so_attrs: ClapAttributes = syn::parse2(tokens).unwrap_or_else(|err| {
+                let tokens_str = if is_empty {
+                    String::new()
+                } else {
+                    format!("problematic tokens: {}", &attr.tts)
+                };
+                panic!("{}, {}", err.to_string(), tokens_str)
+            });
+            s_opt_attrs.extend(so_attrs.attrs);
         }
     }
     s_opt_attrs