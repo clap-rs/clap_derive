TeXitoi/structopt

[Casing] Change default from verbatim to kebab. (#204)
.. fixes TeXitoi/structopt#202

tests/deny-warnings.rs
@@ -46,6 +46,6 @@ fn warning_never_enum() {
         Opt::Foo {
             s: "foo".to_string()
         },
-        Opt::from_iter(&["test", "Foo", "foo"])
+        Opt::from_iter(&["test", "foo", "foo"])
     );
 }

tests/subcommands.rs
@@ -217,7 +217,7 @@ fn flatten_enum() {
 
     assert!(Opt::from_iter_safe(&["test"]).is_err());
     assert_eq!(
-        Opt::from_iter(&["test", "Foo"]),
+        Opt::from_iter(&["test", "foo"]),
         Opt {
             sub_cmd: SubCmd::Foo
         }

clap-rs/clap_derive

tests/deny-warnings.rs
@@ -51,6 +51,6 @@ fn warning_never_enum() {
         Opt::Foo {
             s: "foo".to_string()
         },
-        Opt::parse_from(&["test", "Foo", "foo"])
+        Opt::parse_from(&["test", "foo", "foo"])
     );
 }

tests/subcommands.rs
@@ -213,7 +213,7 @@ fn flatten_enum() {
 
     assert!(Opt::try_parse_from(&["test"]).is_err());
     assert_eq!(
-        Opt::parse_from(&["test", "Foo"]),
+        Opt::parse_from(&["test", "foo"]),
         Opt {
             sub_cmd: SubCmd::Foo
         }