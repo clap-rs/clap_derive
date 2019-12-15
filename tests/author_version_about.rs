TeXitoi/structopt

Fix #283 (#284)
Fix #283

tests/author_version_about.rs
@@ -34,3 +34,17 @@ fn use_env() {
     assert!(output.contains("Guillaume Pinot <texitoi@texitoi.eu>, others"));
     assert!(output.contains("Parse command line argument by defining a struct."));
 }
+
+#[test]
+fn explicit_version_not_str() {
+    const VERSION: &str = "custom version";
+
+    #[derive(StructOpt)]
+    #[structopt(version = VERSION)]
+    pub struct Opt {}
+
+    let mut output = Vec::new();
+    Opt::clap().write_long_help(&mut output).unwrap();
+    let output = String::from_utf8(output).unwrap();
+    assert!(output.contains("custom version"));
+}

clap-rs/clap_derive

tests/author_version_about.rs
@@ -61,3 +61,19 @@ fn use_env() {
     let output = String::from_utf8(output).unwrap();
     assert_eq!(output, ENV_HELP);
 }
+
+#[test]
+fn explicit_version_not_str() {
+    use clap::IntoApp;
+
+    const VERSION: &str = "custom version";
+
+    #[derive(Clap)]
+    #[clap(version = VERSION)]
+    pub struct Opt {}
+
+    let mut output = Vec::new();
+    Opt::into_app().write_long_help(&mut output).unwrap();
+    let output = String::from_utf8(output).unwrap();
+    assert!(output.contains("custom version"));
+}