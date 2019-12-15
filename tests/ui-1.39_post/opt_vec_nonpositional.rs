TeXitoi/structopt

Emit dummy impls on error (#248)

tests/ui-1.39_post/opt_vec_nonpositional.rs
@@ -14,4 +14,7 @@ struct Opt {
     n: Option<Vec<u32>>,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+}

tests/ui-1.39_post/subcommand_opt_opt.rs
@@ -8,7 +8,7 @@
 
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 #[structopt(name = "make-cookie")]
 struct MakeCookie {
     #[structopt(short)]
@@ -18,7 +18,7 @@ struct MakeCookie {
     cmd: Option<Option<Command>>,
 }
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 enum Command {
     #[structopt(name = "pound")]
     /// Pound acorns into flour for cookie dough.
@@ -30,4 +30,7 @@ enum Command {
     },
 }
 
-fn main() {}
+fn main() {
+    let opt = MakeCookie::from_args();
+    println!("{:?}", opt);
+}

tests/ui-common/flatten_and_doc.rs
@@ -24,4 +24,7 @@ struct Opt {
     opts: DaemonOpts,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+}

tests/ui-common/flatten_and_methods.rs
@@ -23,4 +23,7 @@ struct Opt {
     opts: DaemonOpts,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+}

tests/ui-common/non_existent_attr.rs
@@ -15,4 +15,7 @@ struct Opt {
     debug: bool,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+}

tests/ui-common/parse_empty_try_from_os.rs
@@ -15,4 +15,7 @@ struct Opt {
     s: String,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+}

tests/ui-common/skip_with_other_options.rs
@@ -2,11 +2,14 @@ use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "test")]
-pub struct Opts {
+pub struct Opt {
     #[structopt(long)]
     a: u32,
     #[structopt(skip, long)]
     b: u32,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+}

tests/ui-common/subcommand_and_flatten.rs
@@ -8,7 +8,7 @@
 
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 #[structopt(name = "make-cookie")]
 struct MakeCookie {
     #[structopt(short)]
@@ -18,7 +18,7 @@ struct MakeCookie {
     cmd: Command,
 }
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 enum Command {
     #[structopt(name = "pound")]
     /// Pound acorns into flour for cookie dough.
@@ -30,4 +30,7 @@ enum Command {
     },
 }
 
-fn main() {}
+fn main() {
+    let opt = MakeCookie::from_args();
+    println!("{:?}", opt);
+}

clap-rs/clap_derive

tests/ui-1.39_post/opt_vec_nonpositional.rs
@@ -14,4 +14,7 @@ struct Opt {
     n: Option<Vec<u32>>,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::parse();
+    println!("{:?}", opt);
+}

tests/ui-1.39_post/subcommand_opt_opt.rs
@@ -8,7 +8,7 @@
 
 use clap::Clap;
 
-#[derive(Clap)]
+#[derive(Clap, Debug)]
 #[clap(name = "make-cookie")]
 struct MakeCookie {
     #[clap(short)]
@@ -18,7 +18,7 @@ struct MakeCookie {
     cmd: Option<Option<Command>>,
 }
 
-#[derive(Clap)]
+#[derive(Clap, Debug)]
 enum Command {
     #[clap(name = "pound")]
     /// Pound acorns into flour for cookie dough.
@@ -30,4 +30,7 @@ enum Command {
     },
 }
 
-fn main() {}
+fn main() {
+    let opt = MakeCookie::parse();
+    println!("{:?}", opt);
+}

tests/ui-common/flatten_and_doc.rs
@@ -24,4 +24,7 @@ struct Opt {
     opts: DaemonOpts,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::parse();
+    println!("{:?}", opt);
+}

tests/ui-common/flatten_and_methods.rs
@@ -23,4 +23,7 @@ struct Opt {
     opts: DaemonOpts,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::parse();
+    println!("{:?}", opt);
+}

tests/ui-common/non_existent_attr.rs
@@ -15,4 +15,7 @@ struct Opt {
     debug: bool,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::parse();
+    println!("{:?}", opt);
+}

tests/ui-common/parse_empty_try_from_os.rs
@@ -15,4 +15,7 @@ struct Opt {
     s: String,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::parse();
+    println!("{:?}", opt);
+}

tests/ui-common/skip_with_other_options.rs
@@ -2,11 +2,14 @@ use clap::Clap;
 
 #[derive(Clap, Debug)]
 #[clap(name = "test")]
-pub struct Opts {
+pub struct Opt {
     #[clap(long)]
     a: u32,
     #[clap(skip, long)]
     b: u32,
 }
 
-fn main() {}
+fn main() {
+    let opt = Opt::parse();
+    println!("{:?}", opt);
+}

tests/ui-common/subcommand_and_flatten.rs
@@ -8,7 +8,7 @@
 
 use clap::Clap;
 
-#[derive(Clap)]
+#[derive(Clap, Debug)]
 #[clap(name = "make-cookie")]
 struct MakeCookie {
     #[clap(short)]
@@ -18,7 +18,7 @@ struct MakeCookie {
     cmd: Command,
 }
 
-#[derive(Clap)]
+#[derive(Clap, Debug)]
 enum Command {
     #[clap(name = "pound")]
     /// Pound acorns into flour for cookie dough.
@@ -30,4 +30,7 @@ enum Command {
     },
 }
 
-fn main() {}
+fn main() {
+    let opt = MakeCookie::parse();
+    println!("{:?}", opt);
+}