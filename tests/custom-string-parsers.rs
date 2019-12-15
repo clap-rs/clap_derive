TeXitoi/structopt

Pass the try_from_str functions a &str instead of a &String. (#282)
In most cases this doesn't matter, as &String is coerced to a &str, but
this fails for generic functions like CString::new.

tests/custom-string-parsers.rs
@@ -8,7 +8,7 @@
 
 use structopt::StructOpt;
 
-use std::ffi::{OsStr, OsString};
+use std::ffi::{CString, OsStr, OsString};
 use std::num::ParseIntError;
 use std::path::PathBuf;
 
@@ -289,4 +289,18 @@ fn test_custom_bool() {
         },
         Opt::from_iter(&["test", "-dtrue", "-bfalse", "-btrue", "-bfalse", "-bfalse"])
     );
+}
+
+#[test]
+fn test_cstring() {
+    #[derive(StructOpt)]
+    struct Opt {
+        #[structopt(parse(try_from_str = CString::new))]
+        c_string: CString,
+    }
+    assert!(Opt::clap().get_matches_from_safe(&["test"]).is_err());
+    assert_eq!(Opt::from_iter(&["test", "bla"]).c_string.to_bytes(), b"bla");
+    assert!(Opt::clap()
+        .get_matches_from_safe(&["test", "bla\0bla"])
+        .is_err());
 }

clap-rs/clap_derive

tests/custom-string-parsers.rs
@@ -17,7 +17,7 @@ extern crate clap;
 
 use clap::Clap;
 
-use std::ffi::OsStr;
+use std::ffi::{CString, OsStr, OsString};
 use std::num::ParseIntError;
 use std::path::PathBuf;
 
@@ -279,4 +279,19 @@ fn test_custom_bool() {
         },
         Opt::parse_from(&["test", "-dtrue", "-bfalse", "-btrue", "-bfalse", "-bfalse"])
     );
+}
+
+#[test]
+fn test_cstring() {
+    use clap::IntoApp;
+
+    #[derive(Clap)]
+    struct Opt {
+        #[clap(parse(try_from_str = CString::new))]
+        c_string: CString,
+    }
+
+    assert!(Opt::into_app().try_get_matches_from(&["test"]).is_err());
+    assert_eq!(Opt::parse_from(&["test", "bla"]).c_string.to_bytes(), b"bla");
+    assert!(Opt::into_app().try_get_matches_from(&["test", "bla\0bla"]).is_err());
 }