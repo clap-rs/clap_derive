TeXitoi/structopt

Update dependencies (#227)

structopt-derive/src/parse.rs
@@ -193,7 +193,7 @@ pub fn parse_structopt_attributes(all_attrs: &[Attribute]) -> Vec<StructOptAttr>
         .iter()
         .filter(|attr| attr.path.is_ident("structopt"))
         .flat_map(|attr| {
-            let attrs: StructOptAttributes = parse2(attr.tts.clone())
+            let attrs: StructOptAttributes = parse2(attr.tokens.clone())
                 .map_err(|e| match &*e.to_string() {
                     // this error message is misleading and points to Span::call_site()
                     // so we patch it with something meaningful

clap-rs/clap_derive

src/derives/parse.rs
@@ -193,7 +193,7 @@ pub fn parse_clap_attributes(all_attrs: &[Attribute]) -> Vec<ClapAttr> {
         .iter()
         .filter(|attr| attr.path.is_ident("clap"))
         .flat_map(|attr| {
-            let attrs: ClapAttributes = parse2(attr.tts.clone())
+            let attrs: ClapAttributes = parse2(attr.tokens.clone())
                 .map_err(|e| match &*e.to_string() {
                     // this error message is misleading and points to Span::call_site()
                     // so we patch it with something meaningful